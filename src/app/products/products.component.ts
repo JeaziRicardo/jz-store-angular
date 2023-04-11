5fa443ad30fbf1f9cd7453ad65050dbe9ac7bf0c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-11 09:13:46 -0300

    feat: add os produtos através dos métodos do serviço

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { IProduct, products } from '../products';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../products';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-  productsList: IProduct[] = products;
+export class ProductsComponent implements OnInit {
+  productsList: IProduct[] | undefined;
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.productsList = this.productsService.getAll();
+  }
 }
