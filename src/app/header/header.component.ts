423a1bd013aa157fe9f36096099d01309894b0f4
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-12 08:18:11 -0300

    feat: exibe no cabeçalho a quantidade de produtos no carrinho

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  quantity: number = 0;
+
+  constructor(
+    public cartService: CartService
+  ) { }
+
+  quantityCart(): number {
+    return this.quantity = this.cartService.getItems().length; 
+  }
 
 }
