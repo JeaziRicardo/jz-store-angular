84514cb542cffa799d3aacc07ea2044bb25f942c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-13 10:05:55 -0300

    feat: add os dados dos produtos no carrinho

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { ICartProduct } from '../products';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  cartItems: ICartProduct[] = [];
+
+  constructor(
+    public cartService: CartService
+  ) { }
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getItems();
+  }
 }
