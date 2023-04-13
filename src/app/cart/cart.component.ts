0e4f749962045f9a26766a1417868772ac9f31d8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-13 15:25:49 -0300

    feat: exibe o preço total dos produtos no carrinho

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,6 +10,7 @@ import { ICartProduct } from '../products';
 export class CartComponent implements OnInit {
 
   cartItems: ICartProduct[] = [];
+  total: number = 0;
 
   constructor(
     public cartService: CartService
@@ -17,10 +18,17 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getItems();
+    this.totalPrice();
   }
 
   removeItem(product: ICartProduct): void {
     this.cartItems = this.cartItems.filter(item => item.id !== product.id);
     this.cartService.removeItem(product);
+    this.totalPrice();
+  }
+
+  totalPrice(): number {
+    this.total = this.cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+    return this.total;
   }
 }
