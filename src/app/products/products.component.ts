9dc1561c214ad417a2d5ee353548833cd5575da5
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-15 21:08:09 -0300

    feat: exibe os produtos pelos parâmetros da URL

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../products';
 import { ProductsService } from '../products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -11,10 +12,20 @@ export class ProductsComponent implements OnInit {
   productsList: IProduct[] | undefined;
 
   constructor(
-    private productsService: ProductsService
+    private productsService: ProductsService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.productsList = this.productsService.getAll();
+    this.route.queryParams.subscribe(params => {
+      const description = params['description'].toLowerCase();
+
+      if(!description) {
+        this.productsList = this.productsService.getAll();
+        return;
+      }
+
+      this.productsList = this.productsService.getAll().filter(product => product.description.toLowerCase().includes(description));
+    });
   }
 }
